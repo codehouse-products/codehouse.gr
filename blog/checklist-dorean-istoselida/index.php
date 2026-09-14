cd6382a837b90a63a990d4c9f21514bd88308a42
codehouse <infoartivoai@gmail.com>
2026-09-14 22:09:52 +0300

    Redirect retired SEO URL variants

---
+++ blog/checklist-dorean-istoselida/index.php
@@ -0,0 +1,5 @@
+<?php
+declare(strict_types=1);
+
+header('Location: https://codehouse.gr/blog/checklist-dorean-istoselida-epixeirisi/', true, 301);
+exit;
---
+++ dimiourgia-site/index.php
@@ -0,0 +1,5 @@
+<?php
+declare(strict_types=1);
+
+header('Location: https://codehouse.gr/dimioyrgia-site/', true, 301);
+exit;
